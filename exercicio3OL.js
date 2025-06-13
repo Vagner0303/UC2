6106b50116424c1ea21eff9e486cc5eb54ef661c
Vagner Melo Pires <vagnermelopires10@gmail.com>
2025-06-13 16:06:48 -0300

    Delete exercicio3OL.js

--- exercicio3OL.js
+++
@@ -1,6 +0,0 @@
-let valorCompra = 80 // valor total da compra
-let clienteVip = true // se o cliente é VIP ou não
-
-let podeUsarCupom = (valorCompra > 100) || (clienteVip == true)
-
-console.log (podeUsarCupom)
