d40b8489c2ba0dd2725ea344764425e295f4c1da
Vagner Melo Pires <vagnermelopires10@gmail.com>
2025-07-03 14:36:13 -0300

    Add files via upload

--- CardsPerguntas/script.js
+++ CardsPerguntas/script.js
@@ -2,11 +2,25 @@ function mostrarPergunta(tipo){
 let pergunta =""
 if(tipo === "soft"){
 
-pergunta =  "escrever uma pergunta"
+pergunta =  "Qual foi o maior desafio que vocês tiveram?"
 
 }else if(tipo === "hard"){
     
-pergunta =  "escrever uma pergunta"
+pergunta =  "Qual tipo de linguagem de programação que vocês tem mais facilidade?"
+
+}else{
+pergunta = "tipo desconhecido"
+}
+// Mostrar a pergunta na tela dentro da div com id="pergunta"
+document.getElementByld("pewrgunta").textContent = "❓" + pergunta
+// Abre uma caixa de prompt para o usuario digita sua resposta
+const resposta = prompt(pergunta)
+// Verifica se o usuario digitou alguma coisa
+if(resposta){
+// Mostra a resposta na tela 
+document.getElementById("resposta").
+textContent = "Nenhuma resposta fornecida"
 
 }
 }
+
